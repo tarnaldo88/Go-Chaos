329a861178d95571ddf5e07dab0f2dd93b91e658
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-03 11:20:59 -0800

    newtransport implemented

---
+++ internal/proxy/transport.go
@@ -0,0 +1,17 @@
+package proxy
+
+import (
+	"net/http"
+	"time"
+)
+
+func NewTransport() *http.Transport {
+	return &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+}
