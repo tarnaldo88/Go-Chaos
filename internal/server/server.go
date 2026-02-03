27b2265aeb0012192dec289522481edc3d744824
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-03 11:33:54 -0800

    handleConfigGet implemented

--- internal/server/server.go
+++ internal/server/server.go
@@ -75,3 +75,21 @@ func (s *Server) handleConfigUpdate(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte("updated"))
 }
+
+func (s *Server) handleConfigGet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	cfg := s.cfg.Get()
+
+	out, err := yaml.Marshal(cfg)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/x-yaml")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(out)
+}
